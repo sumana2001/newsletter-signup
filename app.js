9140b25f4477e05f3918b1ea519a072c2708c561
sumana2001 <sumana.basu2001@gmail.com>
2020-12-09 00:38:51 +0530

    code

---
+++ app.js
@@ -0,0 +1,15 @@
+const express=require("express");
+const bodyParser=require("body-parser");
+const request=require("request");
+const app=express();
+
+app.use(express.static("public"));
+
+app.get("/",function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.listen(3000,function(){
+    console.log("Server started at port 3000");
+})
